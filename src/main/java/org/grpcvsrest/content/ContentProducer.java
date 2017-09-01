b2ead4eaf170308094632bd53e4596e724de7edc
Mykyta Protsenko <mprotsenko@roku.com>
2017-08-31 19:16:03 -0700

    Added ContentProducer

--- src/main/java/org/grpcvsrest/content/ContentProducer.java
+++ src/main/java/org/grpcvsrest/content/ContentProducer.java
@@ -6,13 +6,28 @@ import com.google.common.io.Resources;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
 
 public class ContentProducer {
     private final List<String> content;
     private int position = 0;
+    private final Random random = new Random();
+
     public ContentProducer(String resourcePath) throws IOException {
         content = Collections.unmodifiableList(
                 Resources.readLines(Resources.getResource(resourcePath), Charsets.UTF_8));
+    }
+
+    public void setCallback(Consumer<String> func) {
+        for(String s: content) {
+            try {
+                Thread.sleep(random.nextInt(200));
+                func.accept(s);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public synchronized String next() {
